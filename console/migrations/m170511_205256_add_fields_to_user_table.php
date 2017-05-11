176ca1999e1e7733a9bb0cd95c8c1aec098968fc
M <mramirez@cubic-lab.com>
2017-05-11 22:53:15 +0200

    [f] create migration to add files to user table

---
+++ console/migrations/m170511_205256_add_fields_to_user_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+class m170511_205256_add_fields_to_user_table extends Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m170511_205256_add_fields_to_user_table cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
