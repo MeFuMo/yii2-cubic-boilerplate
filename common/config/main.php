b48f3e0620406697110f048de12a509d5678fb59
M <mramirez@cubic-lab.com>
2017-05-11 22:19:15 +0200

    [F] add yii2.rbac extension

--- common/config/main.php
+++ common/config/main.php
@@ -1,17 +1,21 @@
 <?php
 
 return [
-	'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
-	'components' => [
-		'cache' => [
-			'class' => 'yii\caching\FileCache',
-		],
-	],
-	'modules' => [
-		'user' => [
-			'class' => 'dektrium\user\Module',
-			'enableFlashMessages' => false,
-			'admins' => ['superadmin']
-		],
-	]
+    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'components' => [
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+            ],
+        'authManager' => [
+            'class' => 'dektrium\rbac\components\DbManager',
+        ],
+    ],
+    'modules' => [
+        'user' => [
+            'class' => 'dektrium\user\Module',
+            'enableFlashMessages' => false,
+            'admins' => ['superadmin']
+        ],
+        'rbac' => 'dektrium\rbac\RbacWebModule',
+    ]
 ];
--- console/config/params.php
+++ console/config/params.php
@@ -3,5 +3,6 @@ return [
 	'adminEmail' => 'admin@example.com',
 	"yii.migrations" => [
 		"@dektrium/user/migrations",
+        "@yii/rbac/migrations"
 	],
 ];
